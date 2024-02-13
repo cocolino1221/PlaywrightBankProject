1203f23f8f0b4f341c272f3c22e81658d2768067
cocolino1221 <pristavita.constantin@gmail.com>
2024-02-13 08:58:34 +0200

    Open page and login test

--- src/main/java/org/example/HomePageBank.java
+++ src/main/java/org/example/HomePageBank.java
@@ -6,14 +6,16 @@ import com.microsoft.playwright.*;
 
 
 public class HomePageBank {
-    Locator title = page.frameLocator(".logo").getByText("Patria Bank");
     Playwright playwright = Playwright.create();
     Browser browser = playwright.firefox().launch();
     Page page = browser.newPage();
 
 
-    public void openPage(){
+
+
+    public void openPage() {
         page.navigate("https://parabank.parasoft.com/parabank/services.htm");
+
     }
 
 
--- src/test/java/HomePageBankTest.java
+++ src/test/java/HomePageBankTest.java
@@ -1,15 +1,40 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.AriaRole;
 import org.example.HomePageBank;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class HomePageBankTest {
-    HomePageBank page;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class HomePageBankTest extends HomePageBank {
+    public static HomePageBank homePage;
+    public static Playwright playwright;
+    public static Browser browser;
+    public static Page page;
+
+    @BeforeAll
+    public static void setup() {
+        playwright = Playwright.create();
+        browser = playwright.firefox().launch();
+        page = browser.newPage();
+        homePage = new HomePageBank();
+
+
+    }
 
 
 
     @Test
     public void openBrowser() {
-        page = new HomePageBank();
-        page.openPage();
+        homePage.openPage();
+        page.getByLabel("Username").fill("asdasd");
+        page.getByLabel("Password").fill("asda");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("LOGIN")).click();
+
+
+
 
 
 
