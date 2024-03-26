14d47e757c612a1ad199d7a25879ff1ff8479cce
cocolino1221 <pristavita.constantin@gmail.com>
2024-03-26 08:07:18 +0200

    register new user

--- src/main/java/org/example/HomePageBank.java
+++ src/main/java/org/example/HomePageBank.java
@@ -9,6 +9,7 @@ public class HomePageBank {
     Playwright playwright = Playwright.create();
     Browser browser = playwright.firefox().launch();
     Page page = browser.newPage();
+    Locator registerButton = page.locator("[name='Register']");
 
 
 
@@ -16,6 +17,9 @@ public class HomePageBank {
     public void openPage() {
         page.navigate("https://parabank.parasoft.com/parabank/services.htm");
 
+    }
+    public void registerButtonClick(){
+        registerButton.click();
     }
 
 
--- src/test/java/HomePageBankTest.java
+++ src/test/java/HomePageBankTest.java
@@ -1,9 +1,12 @@
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.options.AriaRole;
 import org.example.HomePageBank;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
@@ -13,6 +16,9 @@ public class HomePageBankTest extends HomePageBank {
     public static Playwright playwright;
     public static Browser browser;
     public static Page page;
+    BrowserContext context;
+
+
 
     @BeforeAll
     public static void setup() {
@@ -20,24 +26,50 @@ public class HomePageBankTest extends HomePageBank {
         browser = playwright.firefox().launch();
         page = browser.newPage();
         homePage = new HomePageBank();
+        page.navigate("https://parabank.parasoft.com/parabank/services.htm");
 
+    }
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
 
+    @AfterEach
+    void closeContext() {
+        context.close();
     }
 
 
 
     @Test
-    public void openBrowser() {
-        homePage.openPage();
-        page.getByLabel("Username").fill("asdasd");
-        page.getByLabel("Password").fill("asda");
-        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("LOGIN")).click();
+    public void loginFunction() {
 
-
-
-
-
-
+        page.locator("text = Username").fill("john");
+        page.screenshot();
+        page.locator("input[type=\"password\"]").fill("password");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Log In")).click();
+        assertThat(page.getByTitle("Error!")).hasText("An internal error has occurred and has been logged.");
     }
+    @Test
+    public void registerNewUser(){
+        homePage.registerButtonClick();
+        page.locator("[id=\"customer\\.firstName\"]").click();
+        page.locator("[id=\"customer\\.firstName\"]").fill("John");
+        page.locator("[id=\"customer\\.lastName\"]").fill("Wick");
+        page.locator("[id=\"customer\\.address\\.street\"]").fill("USA");
+        page.locator("[id=\"customer\\.address\\.city\"]").fill("LA");
+        page.locator("[id=\"customer\\.address\\.state\"]").fill("California");
+        page.locator("[id=\"customer\\.address\\.zipCode\"]").fill("12345");
+        page.locator("[id=\"customer\\.phoneNumber\"]").fill("98898987");
+        page.locator("[id=\"customer\\.ssn\"]").fill("123312");
+        page.locator("[id=\"customer\\.username\"]").fill("john1221");
+        page.locator("[id=\"customer\\.password\"]").fill("password");
+        page.locator("#repeatedPassword").fill("password");
+        page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("Register")).click();
+       assertThat(page.locator(".title[text='Welcome']").getByText("Welcome John")).hasText("Welcome John");
+    }
+
+
 
 }
